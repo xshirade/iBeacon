b37d5e951532ccb3d99f1903d7557f26104e0761
Satoshi HIRADE <shirade222@gmail.com>
2015-12-25 09:59:40 +0900

    Implemented Basic iBeacon Receiving Part

--- iBeacon/AppDelegate.swift
+++ iBeacon/AppDelegate.swift
@@ -7,40 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
 
-
+    let manager: CLLocationManager = CLLocationManager()
+    let region: CLBeaconRegion = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "704ae405-8122-40d7-a543-249b563b770e")!, identifier: "com.example.www")
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        manager.delegate = self
+        let status = CLLocationManager.authorizationStatus()
+        if status != CLAuthorizationStatus.AuthorizedAlways {
+            manager.requestAlwaysAuthorization()
+        }
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case CLAuthorizationStatus.AuthorizedAlways:
+            print("didChangeAuthorizationStatus: startMonitoringForRegion")
+            manager.startMonitoringForRegion(self.region)
+        default:
+            print("didChangeAuthorizationStatus: stopMonitoringForRegion")
+            manager.stopMonitoringForRegion(self.region)
+        }
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func locationManager(manager: CLLocationManager, didDetermineState state: CLRegionState, forRegion region: CLRegion) {
+        switch state {
+        case CLRegionState.Inside:
+            print("didDetermineState: startRangingBeaconsInRegion")
+            manager.startRangingBeaconsInRegion(self.region)
+        case CLRegionState.Outside, CLRegionState.Unknown:
+            print("didDetermineState: stopRangingBeaconsInRegion")
+            manager.stopRangingBeaconsInRegion(self.region)
+        }
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        // implement whatever you wanna do
+        print(beacons)
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
